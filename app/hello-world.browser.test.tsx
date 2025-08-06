51597adb2a6894b5f273076b8e789c150e3909df
Bart Stefanski <contact@bstefanski.com>
2025-08-06 14:21:02 +0200

    repeat 15

--- app/hello-world.browser.test.tsx
+++ app/hello-world.browser.test.tsx
@@ -4,15 +4,11 @@ import { render } from "vitest-browser-react";
 
 import HelloWorld from "./hello-world";
 
-test(
-  "renders name",
-  async () => {
-    const { getByText, getByRole } = render(<HelloWorld name="Vitest" />);
+test("renders name", { repeats: 15 }, async () => {
+  const { getByText, getByRole } = render(<HelloWorld name="Vitest" />);
 
-    await expect.element(getByText("Hello Vitest x1!")).toBeInTheDocument();
-    await getByRole("button", { name: "Increment " }).click();
+  await expect.element(getByText("Hello Vitest x1!")).toBeInTheDocument();
+  await getByRole("button", { name: "Increment " }).click();
 
-    await expect.element(getByText("Hello Vitest x2!")).toBeInTheDocument();
-  },
-  { repeats: 15 }
-);
+  await expect.element(getByText("Hello Vitest x2!")).toBeInTheDocument();
+});
